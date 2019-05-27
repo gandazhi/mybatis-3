a805bf6cd9893873a99bcca101d20719f72586f7
gandazhi <gandazhi@gmail.com>
2019-05-27 15:40:40 +0800

    修改注释

--- src/main/java/org/apache/ibatis/binding/MapperProxy.java
+++ src/main/java/org/apache/ibatis/binding/MapperProxy.java
@@ -55,7 +55,7 @@ public class MapperProxy<T> implements InvocationHandler, Serializable {
     } catch (Throwable t) {
       throw ExceptionUtil.unwrapThrowable(t);
     }
-    final MapperMethod mapperMethod = cachedMapperMethod(method); // 讲method包装成mybatis人事的MapperMethod
+    final MapperMethod mapperMethod = cachedMapperMethod(method); // 将method包装成mybatis认识的MapperMethod
     return mapperMethod.execute(sqlSession, args);
   }
 
